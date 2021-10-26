1ac398980ba7222f05680053a49f2c5c741a9448
SimoneFranzoni <simofranzoni99@gmail.com>
2021-10-26 16:45:45 +0200

    Dice

--- js/script.js
+++ js/script.js
@@ -47,4 +47,24 @@ for (let i = 0; i <     eMailist.length; i++ ){
 }
 
 
+let pg = Math.floor(Math.random() * 6) + 1;
+let cpu = Math.floor(Math.random() * 6) + 1;
+
+const dicepg = document.getElementById("dicepg");
+dicepg.innerHTML = pg;
+
+const dicecpu = document.getElementById("dicecpu");
+dicecpu.innerHTML = cpu;
+
+if (pg > cpu){
+    const winner = document.getElementById("winner");
+    winner.innerHTML = 'il vincitore è il giocatore';
+} else if (pg < cpu){
+    const winner = document.getElementById("winner");
+    winner.innerHTML = 'il vincitore è il computer';
+} else {
+    const winner = document.getElementById("winner");
+    winner.innerHTML = 'il vincitore non è nessuno, o entrambi, come preferisci';
+}
+
 
